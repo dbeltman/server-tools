45bd78db9f47ff1bd07fc6907b76a2cc4fce8030
Daan B <daan.w.beltman@gmail.com>
2021-06-06 19:46:13 +0200

    refactor stuff for organization, add ambient air temperature sensor

---
+++ components/mqtt_controller.py
@@ -0,0 +1,76 @@
+import json
+import os
+import paho.mqtt.publish as mqtt_publish
+mqtt_host = os.getenv('MQTTHOST', 'MQTT-Server')
+mqtt_port = os.getenv('MQTTPORT', 1883)
+fan_name = os.getenv('FANNAME', 'R510Fan')
+temp_sensor_name = os.getenv('TEMPNAME', fan_name + '-tempsensor')
+
+mqtt_control_topic = os.getenv('CONTROLTOPIC', '' + fan_name + '/fansetpoint')
+mqtt_fanspeed_topic = os.getenv('STATETOPIC', '' + fan_name + '/fanspeed')
+mqtt_client_name = os.getenv('MQTTCLIENTNAME', 'r510-denoiser')
+mqtt_ambient_temp_topic = os.getenv(
+    'TEMPSTATETOPIC', '' + fan_name + '/ambienttemp')
+fan_config_topic = os.getenv('MQTTDISCOVERYTOPIC',
+                             'homeassistant/fan/' + fan_name + '/config')
+temp_config_topic = os.getenv('MQTTDISCOVERYTOPIC',
+                              'homeassistant/sensor/' + temp_sensor_name + '/config')
+
+
+def publish(topic, payload):
+    print("Publishing " +str(payload) + " @" + str(topic))
+    mqtt_publish.single(topic, payload,
+                    hostname=mqtt_host,
+                    client_id=mqtt_client_name,
+                    port=mqtt_port,
+                    retain=True)
+
+
+fan_config_template = {
+    "name": fan_name,
+    "unique_id": fan_name.lower(),
+    "device": {
+        "manufacturer": "Dell",
+        "model": "R510",
+        "name": fan_name,
+        "identifiers": [
+            fan_name.lower()
+        ],
+        "sw_version": "2.0"
+    },
+    "command_topic": mqtt_control_topic,
+    "percentage_state_topic": mqtt_fanspeed_topic,
+    "pct_cmd_t": mqtt_control_topic,
+    "payload_on": "auto",
+    "payload_off": "1"
+}
+temp_config_template = {
+    "name": temp_sensor_name,
+    "unique_id": temp_sensor_name.lower(),
+    "device": {
+        "manufacturer": "Dell",
+        "model": "R510",
+        "name": temp_sensor_name,
+        "identifiers": [
+            temp_sensor_name.lower()
+        ],
+        "sw_version": "2.0"
+    },
+    "state_topic": mqtt_ambient_temp_topic,
+    "unit_of_measurement": "°C"
+}
+# publish fan discovery
+publish(fan_config_topic, json.dumps(fan_config_template))
+# json_template = json.dumps(fan_config_template)
+# mqtt_publish.single(fan_config_topic, json_template,
+#                     hostname=mqtt_host,
+#                     client_id=mqtt_client_name,
+#                     port=mqtt_port,
+#                     retain=True)
+# publish tempsensor discovery
+publish(temp_config_topic, json.dumps(temp_config_template))
+# mqtt_publish.single(temp_config_topic, json.dumps(temp_config_template),
+#                     hostname=mqtt_host,
+#                     client_id=mqtt_client_name,
+#                     port=mqtt_port,
+#                     retain=True)
--- main.py
+++ main.py
@@ -1,64 +1,42 @@
-import paho.mqtt.client as mqtt
 import os
-# from components import SSH_controller
-import paho.mqtt.publish as mqtt_publish
-import json
+from components import mqtt_controller
 from numpy import interp
+from threading import Thread
+from time import sleep
+import paho.mqtt.client as mqtt
 
-mqtt_host = os.getenv('MQTTHOST', 'MQTT-Server')
-mqtt_port = os.getenv('MQTTPORT', 1883)
-fan_name = os.getenv('FANNAME', 'R510Fan')
-mqtt_control_topic = os.getenv('CONTROLTOPIC', '' + fan_name + '/fansetpoint')
-mqtt_state_topic = os.getenv('STATETOPIC', '' + fan_name + '/fanspeed')
-mqtt_client_name = os.getenv('MQTTCLIENTNAME', 'r510-denoiser')
-config_topic = os.getenv('MQTTDISCOVERYTOPIC', 'homeassistant/fan/' + fan_name + '/config')
-# config_template = {
-#     "name": fan_name,
-#     "command_topic": mqtt_control_topic,
-#     "percentage_state_topic": mqtt_state_topic,
-#     "percentage_command_topic": mqtt_control_topic,
-#     "qos": 0,
-#     "payload_on": "auto",
-#     "payload_off": "1",
-# }#
-config_template = {
-  "name": fan_name,
-  "unique_id": fan_name.lower(),
-  "device": {
-    "manufacturer": "Dell",
-    "model": "R510",
-    "name": fan_name,
-    "identifiers": [
-      fan_name.lower()
-    ],
-    "sw_version": "2.0"
-  },
-  "command_topic": mqtt_control_topic,
-  "percentage_state_topic": mqtt_state_topic,
-  "pct_cmd_t": mqtt_control_topic,
-  "payload_on": "auto",
-  "payload_off": "1"
-}
-json_template = json.dumps(config_template)
-mqtt_publish.single(config_topic, json_template,
-                    hostname=mqtt_host,
-                    client_id=mqtt_client_name,
-                    port=mqtt_port,
-                    retain=True)
+def get_ipmi_stats():
+    while True:
+        ipmistats = os.popen("bash /app/dellfanctl.sh simplestats").read()
+        # print(str(ipmistats))
+        ipmistatlist = ipmistats.split("\n")
+        ipmifanspeed = round(int(ipmistatlist[1]) / 11000 * 100)
+        ipmitemp = int(ipmistatlist[0])
+        print("Fan speed: " + str(ipmifanspeed) +
+              "% | Ambient Temperature: " + str(ipmitemp))
+        print("Publishing Fanspeed")              
+        mqtt_controller.publish(mqtt_controller.mqtt_fanspeed_topic, ipmifanspeed)
+        print("Publishing Ambient temperature")                           
+        mqtt_controller.publish(mqtt_controller.mqtt_ambient_temp_topic, ipmitemp)
+        sleep(60)
 
 
-def on_connect(client, userdata, flags, rc):  # The callback for when the client connects to the broker
-    print("Connected with result code {0}".format(str(rc)))  # Print result of connection attempt
+# The callback for when the client connects to the broker
+def on_connect(client, userdata, flags, rc):
+    # Print result of connection attempt
+    print("Connected with result code {0}".format(str(rc)))
 
     client.subscribe(
-        mqtt_control_topic)  # Subscribe to the control topic , receive any messages published on it
+        mqtt_controller.mqtt_control_topic)  # Subscribe to the control topic , receive any messages published on it
 
 
-def on_message(client, userdata, msg):  # The callback for when a PUBLISH message is received from the server.
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
     command = msg.payload.decode('utf-8')
     if command == 'CHECKINSTALL':
         print('Doing nothing for now')
-    print("Message received-> " + msg.topic + ": " + str(msg.payload.decode('utf-8')))  # Print a received msg
+    print("Message received-> " + msg.topic + ": " +
+          str(msg.payload.decode('utf-8')))  # Print a received msg
     payload = msg.payload.decode('utf-8')
 
     if str(payload) == 'auto':
@@ -66,17 +44,21 @@ def on_message(client, userdata, msg):  # The callback for when a PUBLISH messag
         os.system("bash /app/dellfanctl.sh auto")
     elif 0 < int(payload) <= 100:
         payload = int(payload)
-        ##SAFEGUARD For accidental noise discharges
-        safepayload=interp(int(payload), [0,100], [0,60]).round()
-        print("setting to " + str(safepayload) + "original payload was:" + str(payload))
+        # SAFEGUARD For accidental noise discharges
+        safepayload = interp(int(payload), [0, 100], [0, 60]).round()
+        print("setting to " + str(safepayload) +
+              "original payload was:" + str(payload))
         os.system("bash /app/dellfanctl.sh manual")
         os.system("bash /app/dellfanctl.sh set " + str(int(safepayload)))
     else:
         print("Received invalid command, What Do?")
 
 
-client = mqtt.Client(mqtt_client_name)  # Create instance of client with client ID “digi_mqtt_test”
+t = Thread(target=get_ipmi_stats)
+t.start()
+# Create instance of client with client ID “digi_mqtt_test”
+client = mqtt.Client(mqtt_controller.mqtt_client_name)
 client.on_connect = on_connect  # Define callback function for successful connection
 client.on_message = on_message  # Define callback function for receipt of a message
-client.connect(mqtt_host, 1883, 60)
+client.connect(mqtt_controller.mqtt_host, 1883, 60)
 client.loop_forever()  # Start networking daemon
