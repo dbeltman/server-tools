8016aaf4913517ef55b8ba9526527c4db76bc190
Daan B <daan.w.beltman@gmail.com>
2021-10-18 00:01:07 +0200

    improve command repeats and add energy/power sensors

--- components/ipmi_controller.py
+++ components/ipmi_controller.py
@@ -2,6 +2,11 @@ import os
 from numpy import interp
 from components import mqtt_controller
 from time import sleep
+import statistics
+
+def get_stat_value(stat):
+    value=stat.split("|")[1].strip()
+    return value
 
 def get_ipmi_stats():
     while True:
@@ -12,13 +17,38 @@ def get_ipmi_stats():
         ipmitemp = int(ipmistatlist[0])
         print("Fan speed: " + str(ipmifanspeed) +
               "% | Ambient Temperature: " + str(ipmitemp))
-        print("Publishing Fanspeed")              
+        print("Publishing Fanspeed!")              
         mqtt_controller.publish(mqtt_controller.mqtt_fanspeed_topic, ipmifanspeed)
         print("Publishing Ambient temperature")                           
         mqtt_controller.publish(mqtt_controller.mqtt_ambient_temp_topic, ipmitemp)
         sleep(60)
 
-def set_fanmod(mode):
+def scrape_ipmi():
+    while True:
+        statlist = os.popen("bash /app/dellfanctl.sh statlist").read()
+        rpmlist=[]
+        for stat in statlist.split("\n"):
+            if "Ambient" in stat:
+                if "degrees C" in stat:
+                    ambient_temp=get_stat_value(stat)
+                    mqtt_controller.publish(mqtt_controller.mqtt_ambient_temp_topic, ambient_temp)
+                    print ("Ambient temp: " + str(ambient_temp) + " degrees C")
+            elif "FAN MOD 1A" in stat:
+                fanpercent=round((float(get_stat_value(stat)) / 11000 * 100))
+                mqtt_controller.publish(mqtt_controller.mqtt_fanspeed_topic, fanpercent)
+                print (str(fanpercent) + "%")
+            elif "System Level" in stat:
+                wattage=get_stat_value(stat)
+                mqtt_controller.publish(mqtt_controller.mqtt_power_topic, wattage)
+                print (wattage)
+        powerconsumption = os.popen("bash /app/dellfanctl.sh powerconsumption").read()
+        mqtt_controller.publish(mqtt_controller.mqtt_energy_topic, float(powerconsumption))
+
+        print (str(powerconsumption)+ " kWh") 
+        # print(statlist.split("\n"))
+        sleep(30)
+
+def set_fanmode(mode):
     if mode == 'auto':
         print('setting to auto')
         os.system("bash /app/dellfanctl.sh auto")
--- components/mqtt_controller.py
+++ components/mqtt_controller.py
@@ -6,9 +6,14 @@ mqtt_port = os.getenv('MQTTPORT', 1883)
 
 fan_name = os.getenv('FANNAME', 'R510Fan')
 temp_sensor_name = os.getenv('TEMPNAME', fan_name + '-tempsensor')
+power_sensor_name = os.getenv('POWERNAME', fan_name + '-powersensor')
+energy_sensor_name = os.getenv('ENERGYNAME', fan_name + '-energysensor')
 
 mqtt_control_topic = os.getenv('CONTROLTOPIC', '' + fan_name + '/fansetpoint')
 mqtt_fanspeed_topic = os.getenv('STATETOPIC', '' + fan_name + '/fanspeed')
+mqtt_power_topic = os.getenv('POWERSTATETOPIC', '' + power_sensor_name + '/powerusage')
+mqtt_energy_topic = os.getenv('ENERGYSTATETOPIC', '' + power_sensor_name + '/energyusage')
+
 mqtt_client_name = os.getenv('MQTTCLIENTNAME', 'r510-denoiser')
 mqtt_ambient_temp_topic = os.getenv(
     'TEMPSTATETOPIC', '' + fan_name + '/ambienttemp')
@@ -16,7 +21,10 @@ fan_config_topic = os.getenv('MQTTDISCOVERYTOPIC',
                              'homeassistant/fan/' + fan_name + '/config')
 temp_config_topic = os.getenv('MQTTDISCOVERYTOPIC',
                               'homeassistant/sensor/' + temp_sensor_name + '/config')
-
+power_config_topic = os.getenv('MQTTDISCOVERYTOPIC',
+                              'homeassistant/sensor/' + power_sensor_name + '/config')
+energy_config_topic = os.getenv('MQTTDISCOVERYTOPIC',
+                              'homeassistant/sensor/' + energy_sensor_name + '/config')                              
 
 def publish(topic, payload):
     print("Publishing " +str(payload) + " @" + str(topic))
@@ -60,8 +68,44 @@ temp_config_template = {
     "state_topic": mqtt_ambient_temp_topic,
     "unit_of_measurement": "°C"
 }
+power_config_template = {
+    "name": power_sensor_name,
+    "unique_id": power_sensor_name.lower(),
+    "device_class": "power",
+    "device": {
+        "manufacturer": "Dell",
+        "model": "R510",
+        "name": power_sensor_name,
+        "identifiers": [
+            power_sensor_name.lower()
+        ],
+        "sw_version": "2.0"
+    },
+    "state_topic": mqtt_power_topic,
+    "unit_of_measurement": "W"
+}
+energy_config_template = {
+    "name": energy_sensor_name,
+    "unique_id": energy_sensor_name.lower(),
+    "device_class": "energy",
+    "state_class": "total_increasing",
+    "device": {
+        "manufacturer": "Dell",
+        "model": "R510",
+        "name": energy_sensor_name,
+        "identifiers": [
+            energy_sensor_name.lower()
+        ],
+        "sw_version": "2.0"
+    },
+    "state_topic": mqtt_energy_topic,
+    "unit_of_measurement": "kWh"
+}
 # publish fan discovery
 publish(fan_config_topic, json.dumps(fan_config_template))
+publish(power_config_topic, json.dumps(power_config_template))
+publish(energy_config_topic, json.dumps(energy_config_template))
+
 # json_template = json.dumps(fan_config_template)
 # mqtt_publish.single(fan_config_topic, json_template,
 #                     hostname=mqtt_host,
