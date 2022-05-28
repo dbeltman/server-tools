274ae6f0cb5599cd8ed32c2ac0c66b8e082df289
Daan B <daan.w.beltman@gmail.com>
2022-05-28 16:38:47 +0200

    add usrname+ password + remote host support IPMI, bash to python stuff

--- components/ipmi_controller.py
+++ components/ipmi_controller.py
@@ -9,31 +9,19 @@ ipmi_password = os.getenv('IPMIPASSWORD')
 
 if ipmi_host and ipmi_username and ipmi_password:
     print("Remote IPMI interface detected")
-    ipmi_base_command = "ipmitool -H " + ipmi_host + " -U " + ipmi_username + "-P " + ipmi_password
+    ipmi_base_command = "ipmitool -H " + ipmi_host + " -U " + ipmi_username + " -P " + ipmi_password
+    ipmi_oem_base_command = "ipmi-oem -h " + ipmi_host + " -u " + ipmi_username + " -p " + ipmi_password 
 else:
     ipmi_base_command = "ipmitool"
+    ipmi_oem_base_command = "ipmi-oem"
 
 def get_stat_value(stat):
     value=stat.split("|")[1].strip()
     return value
 
-def get_ipmi_stats():
-    while True:
-        ipmistats = os.popen("bash /app/dellfanctl.sh simplestats").read()
-        ipmistatlist = ipmistats.split("\n")
-        ipmifanspeed = round(int(ipmistatlist[1]) / 11000 * 100)
-        ipmitemp = int(ipmistatlist[0])
-        print("Fan speed: " + str(ipmifanspeed) +
-              "% | Ambient Temperature: " + str(ipmitemp))
-        print("Publishing Fanspeed!")              
-        mqtt_controller.publish(mqtt_controller.mqtt_fanspeed_topic, ipmifanspeed)
-        print("Publishing Ambient temperature")                           
-        mqtt_controller.publish(mqtt_controller.mqtt_ambient_temp_topic, ipmitemp)
-        sleep(60)
-
 def scrape_ipmi():
     while True:
-        statlist = os.popen(ipmi_base_command + "sensor").read()
+        statlist = os.popen(ipmi_base_command + " sensor").read()
         for stat in statlist.split("\n"):
             if "Ambient" in stat:
                 if "degrees C" in stat:
@@ -49,7 +37,7 @@ def scrape_ipmi():
                 wattage=get_stat_value(stat)
                 mqtt_controller.publish(mqtt_controller.mqtt_power_topic, wattage)
                 print (wattage)
-        powerconsumption = os.popen("bash /app/dellfanctl.sh powerconsumption").read()
+        powerconsumption = os.popen(ipmi_oem_base_command + " dell get-power-consumption-data | grep kWh | awk '{ print $4}'").read()
         mqtt_controller.publish(mqtt_controller.mqtt_energy_topic, float(powerconsumption))
 
         print (str(powerconsumption)+ " kWh") 
@@ -58,15 +46,19 @@ def scrape_ipmi():
 def set_fanmode(mode):
     if mode == 'auto':
         print('setting to auto')
-        os.system("bash /app/dellfanctl.sh auto")
+        os.system(ipmi_base_command + " raw 0x30 0x30 0x01 0x01")
     else:
         print("Received invalid command, What Do?")
 
 def set_fanspeed(payload):
     payload = int(payload)
     # SAFEGUARD For accidental noise discharges
-    safepayload = interp(int(payload), [0, 100], [0, 60]).round()
+    safepayload = int(interp(int(payload), [0, 100], [0, 60]).round())
+    if len(hex(safepayload)) < 4:
+        hexpayload = hex(safepayload)[:2] + "0" + hex(safepayload)[2:]
+    else:
+        hexpayload = hex(safepayload)
     print("setting to " + str(safepayload) +
             "original payload was:" + str(payload))
-    os.system("bash /app/dellfanctl.sh manual")
-    os.system("bash /app/dellfanctl.sh set " + str(int(safepayload)))
+    os.system(ipmi_base_command + " raw 0x30 0x30 0x01 0x00")
+    os.system(ipmi_base_command + " raw 0x30 0x30 0x02 0xff " + hexpayload)
