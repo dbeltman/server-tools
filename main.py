e93dbdbfaebcd5487eb5e3496d696c364e4ffa1f
dbeltman <36163623+dbeltman@users.noreply.github.com>
2022-05-31 01:23:11 +0200

    try for sub

--- main.py
+++ main.py
@@ -6,8 +6,10 @@ mqtt_subscribe_topics = [(mqtt_controller.mqtt_control_topic ,0),(mqtt_controlle
 
 def on_connect(client, userdata, flags, rc):
     # print("Connected with result code {0}".format(str(rc)))
-
-    client.subscribe(mqtt_subscribe_topics)
+    try:
+        client.subscribe(mqtt_subscribe_topics)
+    except:
+        print("ERROR: Something went wrong subscribing to " + str(mqtt_subscribe_topics))
 
 def on_message(client, userdata, msg):
     print("Message received-> " + msg.topic + ": " +
